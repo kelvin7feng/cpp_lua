8b77676c4a4079648cc8be810488a2c06f7316b7
kelvin7feng <kelvin7.feng@gmail.com>
2016-11-28 15:10:59 +0800

    Initial Commit

---
+++ lua_five_one/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lua_five_one
+//
+//  Created by 冯文斌 on 16/11/28.
+//  Copyright © 2016年 kelvin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
